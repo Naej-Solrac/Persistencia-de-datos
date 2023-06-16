d667a903c0d5300289c0bc32c8658d7ba231e6f7
Jean Gonzales <expretapador3@gmail.com>
2023-06-15 22:06:31 -0500

    creando base de datos

---
+++ src/main/java/com/mycompany/mensajes_app/Mensajes_app.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author 1625360
+ */
+public class Mensajes_app {
+
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        try (Connection cnx = conexion.get_connection()) {
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
