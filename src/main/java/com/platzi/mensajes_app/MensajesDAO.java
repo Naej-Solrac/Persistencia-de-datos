50f1b025c4e3b13a46a473bb7447b3444cfe91d7
Jean Gonzales <expretapador3@gmail.com>
2023-06-15 22:40:15 -0500

    creando metodos CRUD en MensajesDAO

--- src/main/java/com/platzi/mensajes_app/MensajesDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -2,4 +2,17 @@ package com.platzi.mensajes_app;
 //DATA ACCES OBJECT
 //METODOS QUE NOS VAN A PERMITIR CONEXION A BASE DE DATOS
 public class MensajesDAO {
+    public static void CrearMensajeDB(Mensajes mensaje) { //create
+
+    }
+    public static void leerMensajeDB() {    //read
+
+    }
+    public static void actualizarMensajeDB(Mensajes mensaje) {     //update
+
+    }
+    public static void borrarMensajeDB(int id_mensaje) {    //delete
+
+    }
+
 }
