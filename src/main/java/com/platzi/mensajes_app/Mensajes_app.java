f7504f143d2bf8de1b1e0d5fd771f5c6218c8775
Jean Gonzales <expretapador3@gmail.com>
2023-06-15 23:03:21 -0500

    creando el menu en mensajes_app para que selecciones del 1 al 5

--- src/main/java/com/platzi/mensajes_app/Mensajes_app.java
+++ src/main/java/com/platzi/mensajes_app/Mensajes_app.java
@@ -5,6 +5,7 @@
 package com.platzi.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -13,11 +14,40 @@ import java.sql.Connection;
 public class Mensajes_app {
 
     public static void main(String[] args) {
-        Conexion conexion = new Conexion();
-        try (Connection cnx = conexion.get_connection()) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        do {
+            System.out.println("--------");
+            System.out.println("Aplicacion de Mensajes");
+            System.out.println("1. Crear un mensajes");
+            System.out.println("2. Listar mensajes");
+            System.out.println("3. Editar mensajes");
+            System.out.println("4. Eliminar mensajes");
+            System.out.println("5. Salir");
+            opcion = sc.nextInt();
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+            switch (opcion) {
+                case 1:
+                    mensajesService.crearMensaje();
+                    break;
+                case 2:
+                    mensajesService.listarMensaje();
+                    break;
+                case 3:
+                    mensajesService.editarMensaje();
+                    break;
+                case 4:
+                    mensajesService.borrarMensaje();
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion != 5);
+
+//        Conexion conexion = new Conexion();
+//        try (Connection cnx = conexion.get_connection()) {
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
     }
 }
--- src/main/java/com/platzi/mensajes_app/mensajesService.java
+++ src/main/java/com/platzi/mensajes_app/mensajesService.java
@@ -1,4 +1,15 @@
 package com.platzi.mensajes_app;
 //vapa intermedia entre el menu principal y la capa de acceso
 public class mensajesService {
+    public static void  crearMensaje () {
+    }
+    public static void listarMensaje () {
+
+    }
+    public static void  borrarMensaje () {
+
+    }
+    public static void editarMensaje () {
+
+    }
 }
