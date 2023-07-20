6306c4fb056045c2577b88d59f9e7ec970166cee
Jean Gonzales <expretapador3@gmail.com>
2023-07-19 23:33:33 -0500

    CRUD creacion de datos

--- src/main/java/com/platzi/mensajes_app/MensajesDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -1,10 +1,31 @@
 package com.platzi.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 //DATA ACCES OBJECT
 //METODOS QUE NOS VAN A PERMITIR CONEXION A BASE DE DATOS
 public class MensajesDAO {
     public static void CrearMensajeDB(Mensajes mensaje) { //create
+        Conexion db_connect = new Conexion();
+        try (Connection conexion = db_connect.get_connection()) {
+            PreparedStatement ps = null;
+                try {
+                    String query = "INSERT INTO mensajes (mensaje, autor_mensaje ) VALUES (?,?);";
+                    ps=conexion.prepareStatement(query);
+                    ps.setString(1, mensaje.getMensaje());
+                    ps.setString(2, mensaje.getAutor_mensaje());
+                    ps.executeUpdate();
+                    System.out.println("Mensaje creado correctamente");
+                    }   catch (SQLException ex) {
+                        System.out.println(ex);
+                        }
 
-    }
+            }catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
     public static void leerMensajeDB() {    //read
 
     }
--- src/main/java/com/platzi/mensajes_app/mensajesService.java
+++ src/main/java/com/platzi/mensajes_app/mensajesService.java
@@ -1,7 +1,22 @@
 package com.platzi.mensajes_app;
+
+import java.io.StringReader;
+import java.util.Scanner;
+
 //vapa intermedia entre el menu principal y la capa de acceso
 public class mensajesService {
     public static void  crearMensaje () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensajes");
+        String mensaje = sc.nextLine();
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensajes registro  = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+
+        MensajesDAO.CrearMensajeDB(registro);
     }
     public static void listarMensaje () {
 
